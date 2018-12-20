d18bd04dd53dd1e31b4e29cfdde3407b98563615
Leslie Alvarado <lesliemalvarado@gmail.com>
2018-12-20 13:45:39 -0500

    first commit

---
+++ src/main/java/com/leslie/storefront/StoreFrontApplication.java
@@ -0,0 +1,14 @@
+package com.leslie.storefront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreFrontApplication.class, args);
+	}
+
+}
+
