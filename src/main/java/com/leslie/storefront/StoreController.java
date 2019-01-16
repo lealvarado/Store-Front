a4e1f28acababdf022eb8cfba708ae9e505a9ef9
Leslie Alvarado <lesliemalvarado@gmail.com>
2019-01-16 16:35:51 -0500

    Add Controller

--- src/main/java/com/leslie/storefront/StoreController.java
+++ src/main/java/com/leslie/storefront/StoreController.java
@@ -1,5 +1,14 @@
 package com.leslie.storefront;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class StoreController {
+	
+	@GetMapping("/")
+	public String index() {
+		return "testing/index.html";
+	}
 
 }
